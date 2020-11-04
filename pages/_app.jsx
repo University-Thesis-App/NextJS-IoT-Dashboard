2a546c34e75ff73ccc08aca540439fd9aa344f33
Georgia Fasoula <geo@MacBook-Pro-2.local>
2020-11-04 22:12:08 +0200

    make menu global

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,8 +4,84 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../theme';
+import { Button, Typography, Toolbar, AppBar, IconButton, makeStyles, Card, Grid, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Menu, Home as HomeIcon, Person, DevicesOther, ChevronRight } from '@material-ui/icons';
+import clsx from "clsx";
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+  main: {
+    marginTop: 100,
+  },
+  layout: {
+    margin: `0 ${theme.spacing()}`
+  }
+}));
+
 
 export default function MyApp(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
@@ -16,6 +92,14 @@ export default function MyApp(props) {
     }
   }, []);
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <React.Fragment>
       <Head>
@@ -24,7 +108,62 @@ export default function MyApp(props) {
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Component {...pageProps} />
+        <div className={classes.root}>
+          <AppBar position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open
+            })}>
+            <Toolbar>
+              <IconButton
+                className={clsx(classes.menuButton, open && classes.hide)}
+                edge="start" color="inherit" aria-label="menu" onClick={handleDrawerOpen}
+              >
+                <Menu />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                IoT Dashboard
+          </Typography>
+              <Button color="inherit">Logout</Button>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={handleDrawerClose}>
+                <ChevronRight />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button key={"Home"}>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Home"} />
+              </ListItem>
+              <ListItem button key={"Devices"}>
+                <ListItemIcon>
+                  <DevicesOther />
+                </ListItemIcon>
+                <ListItemText primary={"Devices"} />
+              </ListItem>
+              <ListItem button key={"Profile"}>
+                <ListItemIcon>
+                  <Person />
+                </ListItemIcon>
+                <ListItemText primary={"Profile"} />
+              </ListItem>
+            </List>
+          </Drawer>
+          <Component {...pageProps} />
+        </div>
       </ThemeProvider>
     </React.Fragment>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,7 @@
 import { useEffect } from 'react';
 import api from '../api';
 import React from 'react';
-import { Button, Typography, Toolbar, AppBar, IconButton, makeStyles, Card, Grid, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { Menu, Home as HomeIcon, Person, DevicesOther, ChevronRight } from '@material-ui/icons';
-import clsx from "clsx";
-
-const drawerWidth = 240;
+import { makeStyles, Card, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,34 +19,11 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.leavingScreen
     })
   },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
   menuButton: {
     marginRight: theme.spacing(2)
   },
   hide: {
     display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
   },
   content: {
     flexGrow: 1,
@@ -78,8 +51,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home() {
 
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+
   const [devices, setDevices] = React.useState([]);
 
   useEffect(() => {
@@ -93,69 +65,10 @@ export default function Home() {
     // return () => clearInterval(interval);
   }, []);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}>
-        <Toolbar>
-          <IconButton
-            className={clsx(classes.menuButton, open && classes.hide)}
-            edge="start" color="inherit" aria-label="menu" onClick={handleDrawerOpen}
-          >
-            <Menu />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            IoT Dashboard
-          </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronRight />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          <ListItem button key={"Home"}>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary={"Home"} />
-          </ListItem>
-          <ListItem button key={"Devices"}>
-            <ListItemIcon>
-              <DevicesOther />
-            </ListItemIcon>
-            <ListItemText primary={"Devices"} />
-          </ListItem>
-          <ListItem button key={"Profile"}>
-            <ListItemIcon>
-              <Person />
-            </ListItemIcon>
-            <ListItemText primary={"Profile"} />
-          </ListItem>
-        </List>
-      </Drawer>
-      <Grid className={classes.layout} container justify="center" spacing={3}>
+    <div>
+      <Grid justify="center" spacing={3}>
         {devices.map((device) =>
           <Grid item xs={6} >
             <Card key={device.id}>{device.token}</Card>
