381a906fabedda766e0c60201f235687072b1e78
Georgia Fasoula <geo.fasoula@gmail.com>
2020-12-02 22:07:28 +0200

    Added axios default headers

--- api.js
+++ api.js
@@ -3,7 +3,11 @@ import { logOut } from './auth'
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json'
+    }
 });
 
 api.interceptors.response.use(response => response, error => {
